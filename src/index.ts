6038298ba7b616f3e31afd669a62b8030dd6891b
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2021-07-26 01:16:18 -0400

    fix with gts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import{NPCComponent} from './components/npc-component'
+import {NPCComponent} from './components/npc-component';
 export {NPCComponent};
 import {CoinComponent} from './components/coin-component';
 export {CoinComponent};
-import{gameManagerComponent} from './components/game-manager';
-export{gameManagerComponent};
-import{Distractor1Component} from './components/distractor1';
-export{Distractor1Component};
-import{CoinDistractorComponent} from './components/coin-distractor';
-export{CoinDistractorComponent}
-import{CameraComponent} from './components/camera-looking';
-export{CameraComponent};
-import{NPCRotation} from './components/npc-rotation';
-export{NPCRotation}
-import{CameraFrustumComponent} from'./components/CameraFrastum'
-export{CameraFrustumComponent}
-
+import {gameManagerComponent} from './components/game-manager';
+export {gameManagerComponent};
+import {Distractor1Component} from './components/distractor1';
+export {Distractor1Component};
+import {CoinDistractorComponent} from './components/coin-distractor';
+export {CoinDistractorComponent};
+import {CameraComponent} from './components/camera-looking';
+export {CameraComponent};
+import {NPCRotation} from './components/npc-rotation';
+export {NPCRotation};
+import {CameraFrustumComponent} from './components/CameraFrastum';
+export {CameraFrustumComponent};
